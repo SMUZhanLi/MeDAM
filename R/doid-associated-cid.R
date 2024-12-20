3f911fe9c51ebd2da85191142f1945e220a030ae
SMUZhanLi <2194996299@qq.com>
2024-12-20 12:02:56 +0800

    search disease-related compounds

--- R/doid-associated-cid.R
+++ R/doid-associated-cid.R
@@ -5,6 +5,21 @@
 #' @param doid Disease Ontology ID (DOID), e.g. 'DOID:178'.
 #' @details Disease-related compounds collected from Human Metabolome Database
 #' (HMDB) and Comparative Toxicogenomics Database (CTD).
+#' @return A table in which each row represents the compound-disease (cid-doid)
+#' association.
+#' @examples
+#' \dontrun{
+#' # remotes::install_github("rstudio/pool")
+#' library(pool)
+#' library(dplyr)
+#'
+#' medamdb <- dbPool(drv = RSQLite::SQLite(), dbname = "MeDAM.db")
+#' cid <- c("1", "3715")
+#' doid <- c("DOID:83", "DOID:3021")
+#' cid2doid <- drcid_search(medamdb, cid, doid)
+#' poolClose(medamdb)
+#' }
+#' @export
 drcid_search <- function(medam, cid, doid) {
   cid2doid <- medam |>
     dbquery("cid2doid", cid %in% !!cid, doid %in% !!doid)
